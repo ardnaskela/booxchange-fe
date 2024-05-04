539a989c6359471037ba92b9c43f58b7d477c7ac
ardnaskela <alexandra.sergeyeva@gmail.com>
2024-05-04 18:44:05 +0200

    empty constants

--- src/utility/constants.ts
+++ src/utility/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.STRAPI_URL;
-export const TOKEN_KEY = process.env.STRAPI_TOKEN;
+export const API_URL = process.env.STRAPI_URL || '';
+export const TOKEN_KEY = process.env.STRAPI_TOKEN || '';
